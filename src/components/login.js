ff8078001ffce75e075e675ca853484e9cf40638
Ritwik Sharma <39328020+ritrage@users.noreply.github.com>
2019-06-04 17:10:36 +0530

    Update login.js

--- src/components/login.js
+++ src/components/login.js
@@ -4,16 +4,16 @@ import "../App.css";
 class Login extends React.Component {
   render() {
     const lgform = (
-      <form>
+      <form onSubmit="#">
         <input id="login" type="text" placeholder="Login" />
         <input id="password" type="password" placeholder="Password" />
-        <input type="submit" />
+        <input id="submit" type="submit" />
       </form>
     );
     return (
       <div className="App">
         <div className="App-header">Login</div>
-        <div className="App-intro">{lgform}</div>
+        <div className="App-header">{lgform}</div>
       </div>
     );
   }
